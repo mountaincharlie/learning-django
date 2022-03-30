5249f342b89304fd8d21d34a97b9aeef27a9b23f
Charlie <ciharland@hotmail.co.uk>
2022-03-30 11:06:21 +0000

    Add function to redefine how items are named

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,7 @@ from django.db import models
 class Item(models.Model):  # inheriting Django's base Model class
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    # redefining how our items will be named in the db
+    def __str__(self):
+        return self.name
