401ec08c4462a428229607aec159a63be2ee5b9e
Charlie <ciharland@hotmail.co.uk>
2022-03-30 10:29:50 +0000

    Create todo_list.html template and connect to relelvant files

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'  # name of your own apps for Django to look for
 ]
 
 MIDDLEWARE = [
