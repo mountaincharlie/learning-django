83dc115796c69a32eb871bf8eacdfd7f03941fbf
Charlie <ciharland@hotmail.co.uk>
2022-03-30 11:07:01 +0000

    Migrate model and register within admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Item  # our class
 
-# Register your models here.
+# Register your models here (in order to see these items in the admin)
+admin.site.register(Item)
