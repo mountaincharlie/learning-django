84771211953cc99c7c3503ebebf3bf8e096f2489
Charlie <ciharland@hotmail.co.uk>
2022-03-31 09:22:20 +0000

    Create add_item page with views function and url path

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 # Create your views here.
+
+
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')  # returns a HttpResponse
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)  # returns a HttpResponse
+
+
+def add_item(request):
+    if request.method == 'POST':
+        # getting the info from the form
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST  # checking if its been ticked
+        Item.objects.create(name=name, done=done)  # creating the new item
+        return redirect('get_todo_list')  # calling the function to return to home
+
+    return render(request, 'todo/add_item.html')  # returns a HttpResponse
