7d1a98d22d56e4114ed591f3b4ea69c55c28d3e5
Charlie <ciharland@hotmail.co.uk>
2022-04-04 16:34:16 +0000

    Use os.environ.get for setting vars in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*jj0$wvnr)rr=341u=u)zj#j&n1b(rjuyepo+o-%ysl0mf%-4'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-u*jj0$wvnr)rr=341u=u)zj#j&n1b(rjuyepo+o-%ysl0mf%-4')
+# HAVING 2 VARS HERE => second one is a default setting
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
